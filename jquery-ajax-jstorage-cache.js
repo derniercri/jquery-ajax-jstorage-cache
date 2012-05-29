d07010cd18c9932a07ad8eed4297f5114db75d09
Maxime Brazeilles <maxime.brazeilles@gmail.com>
2012-05-29 11:29:27 +0200

    comment cleaning

--- jquery-ajax-jstorage-cache.js
+++ jquery-ajax-jstorage-cache.js
@@ -8,18 +8,16 @@
   };
 
   $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
-    // Cache it ?
     if( options.cacheJStorage  == undefined || ! options.cacheJStorage )
       return;
 
     var cacheKey;
-    // If cacheKey exists we take it, or default one will be used
+
     if ( options.cacheKey )
       cacheKey = options.cacheKey;
     else 
        cacheKey = options.url + options.type + options.data;
     
-    // isCacheValid is a function to validate cache
     if ( options.isCacheValid &&  ! options.isCacheValid() )
       $.jStorage.deleteKey( cacheKey );
     
@@ -27,15 +25,13 @@
       // Do not send a direct copy of Data
       var dataCached = $.extend(true, {}, $.jStorage.get( cacheKey ));
 
-      // In the cache? So get it, apply success callback & abort the XHR request
       if ( options.success )
         options.success( dataCached );
 
-      // Abort is broken on JQ 1.5 :(
+      // Abort is broken on JQ 1.5
       jqXHR.abort();
     }
     else {
-      //If it not in the cache, we change the success callback, just put data on jStorage and after that apply the initial callback
       var successhandler = options.success;
       
       options.success = function( data ) {
